3809c2248d1ac660314163ecee5699a6f0c535dd
Stephen Karpeles <skarpeles@bluechipww.com>
2018-03-08 21:43:04 -0600

    Add base typography

--- homepage.php
+++ homepage.php
@@ -1,7 +1,14 @@
 <?php include('header.php') ?>
 
 
-<p>Hello world! This is HTML5 Boilerplate.</p>
+<h1>h1: Case Study: EFT in Diabetic and Non-Diabetic Pregnancies</h1>
+<h2>h2: Paragraph Heading</h2>
+<h3>h3: Paragraph Heading</h3>
+<h4>h3: Paragraph Heading</h4>
+<p class="intro">
+  intro: Fusce vehicula dolor arcu, sit amet blandit dolor mollis nec. Donec viverra eleifend lacus, vitae ullamcorper metus. Sed sollicitudin ipsum quis nunc sollicitudin ultr.
+</p>
+<p>p: Fusce vehicula dolor arcu, sit amet blandit dolor mollis nec. Donec viverra eleifend lacus, vitae ullamcorper metus. Sed sollicitudin ipsum quis nunc sollicitudin ultrices. Donec euismod scelerisque ligula.</p>
 
 
 
