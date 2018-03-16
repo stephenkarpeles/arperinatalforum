caa1c6cd69c1c95cd70f36f8381f5e0144b55ae0
Stephen Karpeles <skarpeles@bluechipww.com>
2018-03-16 00:46:22 -0500

    Lots o' style updates to homepage

--- homepage.php
+++ homepage.php
@@ -1,18 +1,66 @@
-<?php include('header.php') ?>
+<?php include('inc/header.php') ?>
 
 <section class="hero" style="background: url('img/apf_home-header.jpg') no-repeat; background-size: cover; background-position: center center;">
   Test
 </section>
 
+<section>
+  <div class="container">
+    <div class="content">
+      <p class="intro">The Arkansas Perinatal Forum is an initiative within the Arkansas Department of Health's Family Health Branch that exists to support ongoing activities and workgroups around the state that focus on perinatal health.</p>
+      <h2>Our Mission</h2>
 
-<h1>h1: Case Study: EFT in Diabetic and Non-Diabetic Pregnancies</h1>
-<h2>h2: Paragraph Heading</h2>
-<h3>h3: Paragraph Heading</h3>
-<h4>h3: Paragraph Heading</h4>
-<p class="intro">
-  intro: Fusce vehicula dolor arcu, sit amet blandit dolor mollis nec. Donec viverra eleifend lacus, vitae ullamcorper metus. Sed sollicitudin ipsum quis nunc sollicitudin ultr.
-</p>
-<p>p: Fusce vehicula dolor arcu, sit amet blandit dolor mollis nec. Donec viverra eleifend lacus, vitae ullamcorper metus. Sed sollicitudin ipsum quis nunc sollicitudin ultrices. Donec euismod scelerisque ligula.</p>
+      <div class="grid padded-left">
+        <div class="grid__col">
+          <p>Our mission is to optimize perinatal care and health outcomes for women and infants in Arkansas, by engaging key perinatal stakeholders in a forum that supports the use of data-driven, evidence-based, quality improvement initiatives within the community and health care system.</p>
+        </div>
+        <div class="grid__col">
+          <p>We work to support what's already going on in the state and provide a forum that encourages groups to collaborate, and share information, data, and resources.</p>
+          <div class="cta cta--fancy">
+            <a href="#">Learn more</a>
+          </div>
+        </div>
+      </div><!-- .grid -->
+
+      <h2>Funding</h2>
+
+    </div><!-- .content -->
+  </div><!-- .container -->
+</section>
+
+<section class="cards">
+  <div class="container">
+
+    <div class="grid">
+      <div class="grid__col">
+        <?php include('inc/card.php') ?>
+      </div>
+
+      <div class="grid__col">            
+        <?php include('inc/card.php') ?>
+      </div>
+
+      <div class="grid__col">
+        <?php include('inc/card.php') ?>
+      </div>
+    </div><!-- .grid -->
+
+    <div class="grid">
+      <div class="grid__col">
+        <?php include('inc/card.php') ?>
+      </div>
+
+      <div class="grid__col">            
+        <?php include('inc/card.php') ?>
+      </div>
+
+      <div class="grid__col">
+        <?php include('inc/card.php') ?>
+      </div>
+    </div><!-- .grid -->
+
+  </div>
+</section>
 
 
-<?php include('footer.php') ?>
+<?php include('inc/footer.php') ?>
