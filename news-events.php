a1371f8d7e01e7cca0dd386bbfbaa5ed67f259cf
Stephen Karpeles <stephenk@saltdigitalmarketing.com>
2018-03-24 00:02:27 -0500

    Add filters to news / events page

--- news-events.php
+++ news-events.php
@@ -57,7 +57,7 @@
 </section>
 
 <section class="no-padding-bottom"> 
-  <div class="container--max">
+  <div class="container--max container--flex container--flex-start">
 
     <div class="feat-heading__wrap">
       <div class="feat-heading__img">
@@ -70,6 +70,26 @@
         </div>
       </div>
     </div>    
+
+    <div class="filter">
+      <div class="filter__title">
+        Show:
+      </div>
+      <div class="filter__filters">
+        <div class="filter__choice">
+          <input type="checkbox" id="newsEvents" name="newsevents" value="newsevents">
+          <label for="newsEvents">News &amp; Events</label>
+        </div>
+        <div class="filter__choice">
+          <input type="checkbox" id="newsOnly" name="news" value="news">
+          <label for="newsOnly">News Only</label>
+        </div>
+        <div class="filter__choice">
+          <input type="checkbox" id="eventsOnly" name="events" value="events">
+          <label for="eventsOnly">Events Only</label>
+        </div>
+      </div>
+    </div>
 
   </div>
 </section>
