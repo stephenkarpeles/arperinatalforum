0a08afaf996489b0c6e539c7d18c08874eea198d
Stephen Karpeles <stephenk@saltdigitalmarketing.com>
2018-04-03 13:11:29 -0500

    Trying out a different slider

---
+++ node_modules/tiny-slider/src/tiny-slider.helper.ie8.js
@@ -0,0 +1,11 @@
+import "../bower_components/go-native/src/es5/array/isArray.js";
+import "../bower_components/go-native/src/es5/array/forEach.js";
+import "../bower_components/go-native/src/es5/array/map.js";
+import "../bower_components/go-native/src/es5/array/filter.js";
+import "../bower_components/go-native/src/es5/array/indexOf.js";
+import "../bower_components/go-native/src/ie8/addEventListener.js";
+import "../bower_components/go-native/src/ie8/firstElementChild.js";
+import "../bower_components/go-native/src/ie8/lastElementChild.js";
+import "../bower_components/go-native/src/ie8/previousElementSibling.js";
+import "../bower_components/go-native/src/ie8/nextElementSibling.js";
+import "../bower_components/go-native/src/ie8/getComputedStyle.js";
