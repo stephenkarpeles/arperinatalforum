3b3ee2e617346c8e126299d52b9c7ec8060928ad
Stephen Karpeles <stephenk@saltdigitalmarketing.com>
2018-04-23 10:49:29 -0500

    Backing up previous version

--- wp-content/updraft/themes-old/arperinatal/inc/return-to-top.php
+++
@@ -1,3 +0,0 @@
-<a id="backToTop" href="#top">
-  Return to Top <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-arrow-dark-up.svg" alt="">
-</a>
--- wp-content/updraft/themes-old/arperinatal/news-events-filter.php
+++
@@ -1,7 +0,0 @@
-<?php include('inc/header.php') ?>
-
-<section class="hero" style="background: url('<?php echo get_stylesheet_directory_uri(); ?>/img/apf_home-header.jpg') no-repeat; background-size: cover; background-position: center center;">
-	<div class="hero__overlay"></div>
-</section>
-
-<?php get_footer(); ?>
