9e1567167497a0032544ddc4457ef345e39c8607
Stephen Karpeles <stephenk@saltdigitalmarketing.com>
2018-04-23 00:58:19 -0500

    Redoing some ACF setups

--- wp-content/themes/arperinatal/archive.php
+++ wp-content/themes/arperinatal/archive.php
@@ -13,11 +13,13 @@
 	$website_address 		= get_field('website_address', $term);
 	$facebook_url 			= get_field('facebook_url', $term);
   $twitter_url 				= get_field('twitter_url', $term);
+  $additional_heading	= get_field('additional_heading', $term);
+  $additional_content	= get_field('additional_content', $term);
 
-	$name 						= get_field('name', $term);
-	$abbreviation 		= get_field('abbreviation', $term);
-	$description 			= get_field('description', $term);
-	$region 					= get_field('region', $term);
+	$name 						  = get_field('name', $term);
+	$abbreviation 		  = get_field('abbreviation', $term);
+	$description 			  = get_field('description', $term);
+	$region 					  = get_field('region', $term);
 
 ?>
 
@@ -64,10 +66,13 @@
 			<h2>Latest Updates</h2>
 
 			<div class="grid grid--padding-left half-col-block col-desktop-switch">
+
+				<?php $current_category = single_cat_title("", false); ?>
 				
 				<?php
           $args = array(
             'post_type'   => 'news',
+            'category_name'  => $current_category
            );
            
           $news = new WP_Query( $args );
@@ -99,42 +104,82 @@
                 <?php
               endwhile;
               wp_reset_postdata();
-            ?>                
+            ?> 
+
+            <div class="view-all-block">
+							<a href="">View all updates</a>
+						</div>             
           <?php
           else :
             // no posts found
+            echo '<div class="no-posts-displayed">No updates to display.</div>';
           endif;
-          ?>
+          ?>  
 
-			</div>
-
-			<div class="view-all-block">
-				<a href="">View all updates</a>
 			</div>
 
 			<h2>Resources</h2>
 
 			<div class="grid grid--padding-left half-col-block">
-				<div class="grid__col">
-					<?php include('inc/card-resource.php') ?>
-				</div>
-				<div class="grid__col">
-					<?php include('inc/card-resource.php') ?>
-				</div>
-				<div class="grid__col">
-					<?php include('inc/card-resource.php') ?>
-				</div>
-				<div class="grid__col">
-					<?php include('inc/card-resource.php') ?>
-				</div>
+
+				<?php $current_category = single_cat_title("", false); ?>				
+
+				<?php
+            $args = array(
+              'post_type'   => 'resource',
+              'category_name'  => $current_category
+             );
+             
+            $resources = new WP_Query( $args );
+              if( $resources->have_posts() ) :
+            ?>
+              
+                <?php
+                  while( $resources->have_posts() ) :
+                    $resources->the_post();
+                    ?>
+                      <div class="grid__col">
+                        <div class="card card--resource">
+                          <div class="card__resource-type">
+                            <span><?php the_field('resource_type'); ?></span>
+                          </div>
+                          <div class="card__title">
+                            <h3><?php the_title(); ?></h3>
+                          </div>
+                          <div class="card__content">
+                            <div class="card__description">
+                              <?php the_field('resource_file_name'); ?>
+                            </div>
+                            <div class="card__link">
+                              <a href="<?php the_field('resource_link_url'); ?>"><?php the_field('resource_link_text'); ?></a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    <?php
+                  endwhile;
+                  wp_reset_postdata();
+                ?>  
+
+                <div class="view-all-block">
+									<a href="">View all updates</a>
+								</div>                
+            <?php
+            else :
+              // no posts found
+               echo '<div class="no-posts-displayed">No updates to display.</div>';
+            endif;
+            ?>
+
 			</div>
 
-			<div class="view-all-block">
-				<a href="">View all resources</a>
-			</div>
+			<?php if( get_field('additional_heading') ): ?>
+				<h2><?php echo $additional_heading; ?></h2>
+			<?php endif ?>
 
-			<h2>Partners</h2>
-			<p>Fusce vehicula dolor arcu, sit amet blandit dolor mollis nec. Donec viverra eleifend lacus, vitae ullamcorper metus. Sed sollicitudin ipsum quis nunc sollicitudin ultrices. Donec euismod scelerisque ligula. Maecenas eu varius risus, eu aliquet arcu. Curabitur fermentum suscip.</p>
+			<?php if( get_field('additional_content') ): ?>
+			  <p><?php echo $additional_content; ?></p>
+			<?php endif ?>
 
 			<div class="hide-above-ipad">
 			  <?php include('inc/event-listing.php') ?>			
