f6bc2fcaa5e6d18e29bf45d11af68a8dce00dae2
Stephen Karpeles <stephenk@saltdigitalmarketing.com>
2018-04-23 01:57:12 -0500

    Update homepage news links

--- wp-content/themes/arperinatal/functions.php
+++ wp-content/themes/arperinatal/functions.php
@@ -293,3 +293,25 @@ function arp_post_types_init() {
 	register_post_type( 'resource', $resource_args );
 
 }
+
+
+/**
+ * Add CPTs to search
+ */
+add_filter( 'pre_get_posts', 'custom_cpt_search' );
+/**
+ * This function modifies the main WordPress query to include an array of 
+ * post types instead of the default 'post' post type.
+ *
+ * @param object $query  The original query.
+ * @return object $query The amended query.
+ */
+function custom_cpt_search( $query ) {
+	
+    if ( $query->is_search ) {
+	$query->set( 'post_type', array( 'event', 'resource', 'news' ) );
+    }
+    
+    return $query;
+    
+}
